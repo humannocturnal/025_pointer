b66d7c33d823a27fac94308d2dbb043c3217b926
humannocturnal <syaputrar374@gmail.com>
2023-05-10 16:23:31 +0700

    Add project files.

---
+++ pointer/pointer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa {
+public:
+	int nim;
+	void showNim(); //deklarasi method
+};
+
+void mahasiswa::showNim() { // implementasi method diluar class
+	cout << "No Induk = " << nim << endl;
+}
+
+int main()
+{
+	mahasiswa mhs{ 1 };  //object mhs
+	mhs.showNim();       //Member Access Operator
+
+	mahasiswa& ref = mhs; //Pointer Reference refMhs
+	ref.nim = 2;          //Member Acces Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; //Pointer Dereference pMhs
+	pMhs->nim = 3;
+	mhs.showNim();
+	system("pause");
+	return 0;
+}
